e221f22408185400791b630aa02b6105362b9e9e
Igor Savin <iselwin@gmail.com>
2022-12-26 14:04:54 +0200

    Make TS type less restrictive (#160)

--- test/AsyncTask.spec.ts
+++ test/AsyncTask.spec.ts
@@ -43,6 +43,50 @@ describe('ToadScheduler', () => {
       })
     })
 
+    it('correctly handles async errors with Promise.all', (done) => {
+      unMockTimers()
+      expectAssertions(3)
+      let error: string
+      let result1: boolean
+      let result3: boolean
+      const scheduler = new ToadScheduler()
+      const task = new AsyncTask(
+        'async task',
+        () => {
+          const promise1 = Promise.resolve().then(() => {
+            result1 = true
+          })
+          const promise2 = Promise.resolve().then(() => {
+            throw new Error('kaboom')
+          })
+          const promise3 = Promise.resolve().then(() => {
+            result3 = true
+          })
+          return Promise.all([promise1, promise2, promise3])
+        },
+        (err: Error) => {
+          error = err.message
+        }
+      )
+      const job = new SimpleIntervalJob(
+        {
+          seconds: 1,
+          runImmediately: true,
+        },
+        task
+      )
+
+      scheduler.addSimpleIntervalJob(job)
+
+      sleep(10).then(() => {
+        expect(error).toBe('kaboom')
+        expect(result1).toBe(true)
+        expect(result3).toBe(true)
+        scheduler.stop()
+        done()
+      })
+    })
+
     it('correctly handles errors asynchronously', (done) => {
       unMockTimers()
       expectAssertions(1)
