8a10f0c34e58adf5340fa93abe20bd6fdf43f733
Igor Savin <iselwin@gmail.com>
2026-04-28 01:31:57 +0300

    Fix karma tests

--- karma.conf.js
+++ karma.conf.js
@@ -6,6 +6,27 @@ module.exports = function (config) {
       "**/*.ts": "karma-typescript"
     },
     reporters: ['progress', 'karma-typescript'],
+    karmaTypescriptConfig: {
+      bundlerOptions: {
+        acornOptions: {
+          ecmaVersion: 'latest',
+        },
+        resolve: {
+          alias: {
+            croner: require('path').resolve(__dirname, 'node_modules/croner/dist/croner.cjs'),
+          },
+          extensions: ['.js', '.json', '.mjs', '.cjs', '.ts', '.tsx'],
+        },
+        transforms: [
+          function (context, callback) {
+            if (context.filename && context.filename.endsWith('.cjs')) {
+              return callback(undefined, { dirty: true, transformedScript: true })
+            }
+            return callback(undefined, { dirty: false })
+          },
+        ],
+      },
+    },
     port: 9876, // karma web server port
     colors: true,
     logLevel: config.LOG_INFO,
